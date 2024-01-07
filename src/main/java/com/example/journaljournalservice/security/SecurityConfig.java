adb010af8db89f6bab79f79a5fd1fbc1926f4da4
CaffeQ <102230900+CaffeQ@users.noreply.github.com>
2024-01-07 14:45:49 +0100

    added cors for frontend url

--- src/main/java/com/example/journaljournalservice/security/SecurityConfig.java
+++ src/main/java/com/example/journaljournalservice/security/SecurityConfig.java
@@ -15,8 +15,12 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -30,11 +34,17 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.cors(AbstractHttpConfigurer::disable);
+
+        http.cors(core->{
+            core.configurationSource(corsConfigurationSource());
+        });
+
         http.csrf(AbstractHttpConfigurer::disable);
 
         http.authorizeHttpRequests(authorize -> {
 
             authorize
+                    .requestMatchers("/").permitAll()
                     .requestMatchers(HttpMethod.POST,"/journal/staff").permitAll()
                     .requestMatchers(HttpMethod.POST,"/journal/patient").permitAll()
                     .requestMatchers(HttpMethod.OPTIONS,"/**").permitAll()
@@ -77,6 +87,28 @@ public class SecurityConfig {
         });
         return jwtAuthenticationConverter;
     }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(
+                System.getenv("FRONTEND_URL"),
+                System.getenv("ACCOUNT_SERVICE_URL")
+        )); // Adjust as necessary
+        //configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // Adjust as necessary
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type","Access-Control-Allow-Origin","userCookieID"));
+        configuration.addExposedHeader("Access-Control-Allow-Credentials");
+        configuration.addExposedHeader("Access-Control-Allow-Origin");
+        configuration.addExposedHeader("Authorization");
+        configuration.addExposedHeader("userCookieID");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source; // This is actually an instance of UrlBasedCorsConfigurationSource
+    }
+
 
 
 
